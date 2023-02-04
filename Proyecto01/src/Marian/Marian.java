e12eb5e9272cd3c53bc328a494f89e518a3963eb
Maria Antònia Fernández Mateu <mafernandezmateu@gmail.com>
2023-02-04 01:27:03 +0100

    Paso7

---
+++ Proyecto01/src/Marian/Marian.java
@@ -0,0 +1,11 @@
+package Marian;
+
+public class Marian {
+
+	public static void main(String[] args) {
+		Saludo();
+	}
+	private static void Saludo() {
+		System.out.println("Hola, soy Maria Antònia");
+	}
+}
