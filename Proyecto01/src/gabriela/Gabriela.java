27a318e2f91b6daa63ab9de3ef03d9b57f761a5a
Gabriela.Prieto.Herrera <gabiprieto7@gmail.com>
2023-02-04 08:42:37 +0000

    Añadido paquete y clase java

---
+++ Proyecto01/src/gabriela/Gabriela.java
@@ -0,0 +1,12 @@
+package gabriela;
+
+public class Gabriela {
+
+	public static void main(String[] args) {
+		Saludo();
+	}
+	private static void Saludo() {
+		System.out.println("Hola, soy Gabriela");
+	}
+
+}
