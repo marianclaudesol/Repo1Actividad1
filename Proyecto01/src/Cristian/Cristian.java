9e25fbdb07574895f02c196f22dd78e0c8c41dac
Cristian.Quiceno <cristian.qprog@gmail.com>
2023-02-04 10:14:31 +0100

    add método, clase y paquete Cristian

---
+++ Proyecto01/src/Cristian/Cristian.java
@@ -0,0 +1,15 @@
+package Cristian;
+
+public class Cristian {
+public static void main(String[] args) {
+	
+	saludo("Cristian");
+	
+}
+
+public static void saludo (String nombre) {
+	System.out.println("Hola, soy " + nombre);
+	
+}
+	
+}
