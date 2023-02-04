06d3f7768352659f09130d7d52ee67b8910d2647
Gabriela.Prieto.Herrera <gabiprieto7@gmail.com>
2023-02-04 10:00:14 +0000

    add Hobbies

--- Proyecto01/src/gabriela/Gabriela.java
+++ Proyecto01/src/gabriela/Gabriela.java
@@ -4,9 +4,16 @@ public class Gabriela {
 
 	public static void main(String[] args) {
 		Saludo();
+		Hobbies();
 	}
 	private static void Saludo() {
 		System.out.println("Hola, soy Gabriela");
 	}
+	
+	private static void Hobbies() {
+		System.out.println("Mis tres Hobbies son: " + "- Natación" 
+	+ " - Senderismo " + "- Las rutas en moto ");
+	}
+	
 
 }
